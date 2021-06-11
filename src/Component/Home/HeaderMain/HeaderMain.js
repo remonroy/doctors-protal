17d420743e792a6f7c68a1593d72b1dda8e76763
remonroy <remonroy34@gmail.com>
2021-06-11 19:21:27 +0600

    heroku second commit

--- src/Component/Home/HeaderMain/HeaderMain.js
+++ src/Component/Home/HeaderMain/HeaderMain.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import chair from '../../../images/chair.png';
+import { useHistory } from "react-router-dom";
 const HeaderMain = () => {
+    let history = useHistory();
+    const handleClick=()=>{
+        history.push("/appoinment");
+    }
     return (
         <main style={{height:'600px'}} className="row d-flex align-items-center">
             <div className="col-md-4 offset-md-1">
                 <h1>Your new smile <br/> starts hear</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta nisi odit rerum molestiae reiciendis repudiandae?</p>
                 {/* <a href className="btn btn-primary">Get appionment</a> */}
-                <a href="https://quiet-ocean-01238.herokuapp.com/appoinment" className="btn btn-primary">Get appionment</a>
+                <button onClick={handleClick} className="btn btn-primary">Get appionment</button>
             </div>
             <div className="col-md-6">
                 <img src={chair} alt="" className="img-fluid"/>
